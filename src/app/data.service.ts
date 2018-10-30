281dc05b4cff0fd52162d2fb23d64060cfa37fcd
booshay <boucher_08641@yahoo.com>
2018-10-29 20:55:10 -0400

    first commit

---
+++ src/app/data.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  constructor(private http: HttpClient) { }
+ 
+getBeer(){
+
+  let data = {
+     // query:"{beer(id: "+idNum+") {name,abv,description,imageUrl}}",
+      query:"  {topBeers{items{id, name, style {description}, description, overallScore, imageUrl, abv, brewer {name, facebook, web}}}}",
+      variables:"{}",
+      operationName:null
+  };
+
+  return this.http.post('https://api.r8.beer/v1/api/graphql/', data, {
+  headers:{
+     'x-api-key': 'yo5e6HwJhh4xzYm1uTUO9mdHTmAQ6TL7w5wfMNNg'}
+    });
+}
+}
+/*
+curl -v 'https://api.r8.beer/v1/api/graphql/' \
+-H 'content-type: application/json' \
+-H 'accept: application/json' \
+-H 'x-api-key: yo5e6HwJhh4xzYm1uTUO9mdHTmAQ6TL7w5wfMNNg' \
+--data-binary '{"query":"query {\n beer(id: 4934) {\n id\n name\n }\n}","variables":"{}","operationName":null}'
+*/
---
+++ src/app/view/view.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import {DataService} from '../data.service'
+
+@Component({
+  selector: 'app-view',
+  templateUrl: './view.component.html',
+  styleUrls: ['./view.component.css']
+})
+export class ViewComponent implements OnInit {
+  beers: {};
+  constructor(private dataService: DataService) { }
+
+  ngOnInit() {
+  this.dataService.getBeer()
+   .subscribe(data=>console.log(this.beers = data));
+  
+  }
+}
