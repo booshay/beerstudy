e8ae64e84abd5762641237b66dba9a9cb2764fbb
Mark Boucher <boucher_08641@yahoo.com>
2018-11-01 19:55:56 +0000

    seperated HTML from view to detail components

--- src/app/beer-detail/beer-detail.component.ts
+++ src/app/beer-detail/beer-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-beer-detail',
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./beer-detail.component.css']
 })
 export class BeerDetailComponent implements OnInit {
-
+  @Input() beer: {};
   constructor() { }
 
   ngOnInit() {
