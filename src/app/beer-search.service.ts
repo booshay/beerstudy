17a974452c5d9051ed29ee7affef538b3b38bf93
booshay <boucher_08641@yahoo.com>
2018-11-25 19:14:51 -0500

    moved search to navbar, works on homepage

--- src/app/beer-search.service.ts
+++ src/app/beer-search.service.ts
@@ -1,26 +1,34 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+import * as Rx from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class BeerSearchService {
+  
+  private _beers = new Rx.BehaviorSubject (null);
 
   constructor(private http: HttpClient) { }
+ 
 
-  searchBeer(name){
+  searchBeer(name:string){
    
     let data= {
       query: `{beerSearch(query:"`+name+`"){items{id, name, style {description}, description, overallScore,
        imageUrl, abv, brewer {name, facebook, web}}}}`,
       variables:"{}",
       operationName:null
-    }
+    };
 
     return this.http.post('https://api.r8.beer/v1/api/graphql/', data, {
-      headers:{
-         'x-api-key': 'yo5e6HwJhh4xzYm1uTUO9mdHTmAQ6TL7w5wfMNNg'}
-        });
-  }
+      headers:{'x-api-key': 'yo5e6HwJhh4xzYm1uTUO9mdHTmAQ6TL7w5wfMNNg'}
+    }).subscribe(data => {
+      this._beers.next(data);
+    });
+}
 
+get beers$() {
+  return this._beers.asObservable();
+}
 }
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {BeerSearchService} from '../beer-search.service';
+
 
 @Component({
   selector: 'app-navbar',
@@ -7,8 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private beerSearchService:BeerSearchService) { }
+  
+  searchBeer(beerName) {
+    this.beerSearchService.searchBeer(beerName);
+  }
+  
   ngOnInit() {
   }
 
